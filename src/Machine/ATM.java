5bd0899a9d1a529415eb079a51cb4ea75864530c
Collins Chacha <chacollins19@gmail.com>
2024-10-08 09:36:18 +0300

    first commit

---
+++ src/Machine/ATM.java
@@ -0,0 +1,10 @@
+package Machine;
+
+import java.io.IOException;
+
+public class ATM extends OptionMenu{
+    public static void main(String [] args) throws IOException {
+        OptionMenu optionMenu = new OptionMenu();
+        optionMenu.getLogin();
+    }
+}
